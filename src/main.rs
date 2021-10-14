93e95eaa0bfaabe58a643a81e43d3f7963758296
Teo Chikhany <47039131+teochikhany@users.noreply.github.com>
2021-10-14 16:18:56 +0300

    get the bytes forming the captcha image

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
-use std::process::{Command, Stdio};
-use std::io::{BufRead, Write, BufReader};
-use std::vec::Vec;
-use std::{thread, time};
+extern crate image;
+extern crate hex;
+
+use std::process::Command;
 use std::str;
+use wait_timeout::ChildExt;
+use std::time::Duration;
+
 
 fn main() {
     
     println!("Hello, world!");
 
     // .\tshark.exe -i wifi -w AAA.pcap -f "tcp and host 193.227.187.169"
-    let mut tshark_capture = Command::new("D:\\Apps\\wireshark\\tshark.exe")
+    let _tshark_capture = Command::new("D:\\Apps\\wireshark\\tshark.exe")
                             .args(["-i", "wifi", "-w", "BAA.pcap", "-f", "tcp and host 193.227.187.169" ])
-                            // .stdout(Stdio::piped())
                             .spawn()
-                            .expect("tshark command failed to start");
+                            .expect("tshark command failed to start")
+                            .wait_timeout(Duration::from_secs(10));
 
-    thread::sleep(time::Duration::from_secs(5));
+    println!("finished waiting");
 
-    tshark_capture.kill().unwrap();
-
-    // .\tshark.exe -r .\AAA.pcap -c 13 -x
+    // D:\\Apps\\wireshark\\tshark.exe -r .\BAA.pcap -T json -Y 'http' -x > test.json
     let tshark_analyse = Command::new("D:\\Apps\\wireshark\\tshark.exe")
-                .args(["-r", "BAA.pcap", "-x"])
+                .args(["-r", "BAA.pcap", "-T", "json", "-Y", "http", "-x"])
                 .output()
                 .expect("tshark command failed to start");
 
+    let res = str::from_utf8(&tshark_analyse.stdout).unwrap();
 
-    let res = &tshark_analyse.stdout;
-    let char2: Vec<char> = res.iter().map(|b| *b as char).collect::<Vec<_>>();
+    let responce = json::parse(res).unwrap();
 
-    for i in 0 .. char2.len() - 4
-    {
-        // print!("{:?} ", char2[i]);
+    println!("responce: {}", responce[1]["_source"]["layers"]["png_raw"][0]);
 
-        if res[i] == 0x89 && res[i+1] == 0x50 && res[i+2] == 0x4E
-        {
-            println!("found one png");
-        }
-    }
+    let image = &responce[1]["_source"]["layers"]["png_raw"][0].as_str().unwrap();
+
+    let buffer = hex::decode(image).expect("Decoding failed");
+
+    println!("responce2 : {:?}", &buffer);
+
+    image::save_buffer("image.png", &buffer, 120, 30, image::ColorType::Rgb8).unwrap();
 
 }
