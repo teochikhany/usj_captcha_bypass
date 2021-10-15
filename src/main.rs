1037b35191b8c5da23f16ed9481fc7ff57bd8a30
Teo Chikhany <47039131+teochikhany@users.noreply.github.com>
2021-10-15 10:00:22 +0300

    can capture the captcha image

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,66 @@ use std::process::Command;
 use std::str;
 use wait_timeout::ChildExt;
 use std::time::Duration;
+// use image::io::Reader as ImageReader;
+// use image::DynamicImage;
+// use image::ImageBuffer;
+use std::fs::File;
+use std::io::prelude::*;
 
 
 fn main() {
     
     println!("Hello, world!");
 
-    // .\tshark.exe -i wifi -w AAA.pcap -f "tcp and host 193.227.187.169"
+    start_capture();
+
+    println!("finished waiting");
+
+    let tshark_analyse = parse_pcap();
+
+    extract_image(tshark_analyse);
+
+}
+
+/// Start the capture with tshark using the following command :
+///     .\tshark.exe -i wifi -w AAA.pcap -f "tcp and host 193.227.187.169"
+fn start_capture()
+{
     let _tshark_capture = Command::new("D:\\Apps\\wireshark\\tshark.exe")
                             .args(["-i", "wifi", "-w", "BAA.pcap", "-f", "tcp and host 193.227.187.169" ])
                             .spawn()
                             .expect("tshark command failed to start")
                             .wait_timeout(Duration::from_secs(10));
+}
 
-    println!("finished waiting");
 
-    // D:\\Apps\\wireshark\\tshark.exe -r .\BAA.pcap -T json -Y 'http' -x > test.json
-    let tshark_analyse = Command::new("D:\\Apps\\wireshark\\tshark.exe")
-                .args(["-r", "BAA.pcap", "-T", "json", "-Y", "http", "-x"])
-                .output()
-                .expect("tshark command failed to start");
+/// Parse the pcap file with tshark using the following command
+/// D:\\Apps\\wireshark\\tshark.exe -r .\BAA.pcap -T json -Y 'http' -x > test.json
+fn parse_pcap() -> std::process::Output
+{
+    return Command::new("D:\\Apps\\wireshark\\tshark.exe")
+                    .args(["-r", "BAA.pcap", "-T", "json", "-Y", "http", "-x"])
+                    .output()
+                    .expect("tshark command failed to start");
+}
 
-    let res = str::from_utf8(&tshark_analyse.stdout).unwrap();
 
-    let responce = json::parse(res).unwrap();
+/// read the json file parsed from the pcap and 
+/// save the latest image with the dimentions of 120x30
+fn extract_image(tshark_analyse: std::process::Output)
+{
+    let res = str::from_utf8(&tshark_analyse.stdout).expect("could not cast output as utf8");
 
-    println!("responce: {}", responce[1]["_source"]["layers"]["png_raw"][0]);
+    let responce = json::parse(res).expect("could not parse the json file");
+
+    // println!("responce: {}", responce[1]["_source"]["layers"]["png_raw"][0]);
 
     let image = &responce[1]["_source"]["layers"]["png_raw"][0].as_str().unwrap();
 
     let buffer = hex::decode(image).expect("Decoding failed");
 
-    println!("responce2 : {:?}", &buffer);
+    // println!("responce2 : {:?}", &buffer);
 
-    image::save_buffer("image.png", &buffer, 120, 30, image::ColorType::Rgb8).unwrap();
-
+    let mut file = File::create("captcha.png").expect("could do create file");
+    file.write_all(&buffer).expect("could not write to file");
 }
