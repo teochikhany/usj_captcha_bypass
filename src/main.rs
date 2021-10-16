6098adc977eb949a7f972607eaed0d85027556ee
Teo Chikhany <47039131+teochikhany@users.noreply.github.com>
2021-10-16 16:23:31 +0300

    added captchas unit test

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate image;
 use image::open;
 use std::fs::File;
 use std::io::prelude::*;
-use clipboard_win::{formats, get_clipboard};
-// use image::{RgbImage, Rgb, ImageFormat};
+
+#[allow(unused_imports)]
+use clipboard_win::{formats, get_clipboard, set_clipboard_string};
 
 /// just an alias to simplify image parameters
 type IMAGE = image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>;
@@ -29,10 +30,15 @@ fn main() {
     // black pixels for each number
     let nb_black = read_image(&img);
 
+    let mut result: String = "".to_owned();
+
     for i in 0 .. 5
     {
-        get_nb(nb_black[i], &img, i as u32);
+        let nb : &str = get_nb(nb_black[i], &img, i as u32);
+        result.push_str(nb);
     }
+
+    println!("result: {}", result);
 }
 
 
@@ -151,17 +157,19 @@ fn get_first_pixel(img: &IMAGE, iter:u32) -> (u32, u32)
 
 
 /// Determening wich number is in the current section
-fn get_nb(nb_black : u16, img: &IMAGE, iter:u32)
+/// depending on the numbers of black pixel in each section
+/// and the postion of pixel depending on the first black pixel
+fn get_nb(nb_black : u16, img: &IMAGE, iter:u32) -> &str
 {
     match nb_black
     {
-        36 => println!("0"),
-        16 => println!("1"),
-        21 => println!("2"),
-        37 => println!("5"),
-        15 => println!("7"),
-        40 => println!("8"),
-        29 => println!("5"),
+        36 => "0",
+        16 => "1",
+        21 => "2",
+        37 => "5",
+        15 => "7",
+        40 => "8",
+        29 => "5",
         20 => or_0_7_4(img, iter),
         27 => or_1_2_4(img, iter),
         39 => or_9_6(img, iter),
@@ -170,142 +178,253 @@ fn get_nb(nb_black : u16, img: &IMAGE, iter:u32)
         22 => or_3_9(img, iter),
         23 => or_5_6(img, iter),
         26 => or_3_8_7(img, iter),
-        _ => println!("unknows nb of black pixel: {}", nb_black)
+        _ => { println!("unknows nb of black pixel: {}", nb_black); return "R" }
     }
 }
 
 
-fn or_0_7_4(img: &IMAGE, iter:u32)
+fn or_0_7_4(img: &IMAGE, iter:u32) -> &str
 {
     let first_black = get_first_pixel(img, iter);
 
     if img.get_pixel(first_black.0 + 4, first_black.1 - 4) == &BLACK_PIXEL 
     {
-        print!("4 \t")
+        return "4";
     }
     else if img.get_pixel(first_black.0 + 3, first_black.1 + 3) == &BLACK_PIXEL 
     {
-        print!("7 \t")
+        return "7";
     }
     else
     {
-        print!("0 \t")
+        return "0";
     }
 
-    println!("either _ or _ or _ (4, 7, 0)");
+    // println!("either _ or _ or _ (4, 7, 0)");
 }
 
 
-fn or_1_2_4(img: &IMAGE, iter:u32)
+fn or_1_2_4(img: &IMAGE, iter:u32) -> &str
 {
     let first_black = get_first_pixel(img, iter);
 
     if img.get_pixel(first_black.0 + 4, first_black.1 - 4) == &BLACK_PIXEL
     {
-        println!("4 \t")
+        return "4"
     }
     else if img.get_pixel(first_black.0 + 2, first_black.1 - 2) == &BLACK_PIXEL
         && img.get_pixel(first_black.0 + 1, first_black.1 - 1) == &BLACK_PIXEL
     {
-        println!("1 \t")
+        return "1"
     }
     else if img.get_pixel(first_black.0 + 2, first_black.1 - 2) != &BLACK_PIXEL
                 && img.get_pixel(first_black.0 + 1, first_black.1 - 1) == &BLACK_PIXEL
     {
-        println!("2 \t")
+        return "2"
     }
+
+    return "X"
 }
 
 
-fn or_2_8(img: &IMAGE, iter:u32)
+fn or_2_8(img: &IMAGE, iter:u32) -> &str
 {
     let first_black = get_first_pixel(img, iter);
 
     if img.get_pixel(first_black.0 + 7, first_black.1 + 7) == &BLACK_PIXEL 
     {
-        println!("2 \t")
+        return "2"
     }
     else
     {
-        println!("8 \t")
+        return "8"
     }
 }
 
 
 #[allow(unused_variables)]
-fn or_3_9(img: &IMAGE, iter:u32)
+fn or_3_9(img: &IMAGE, iter:u32) -> &str
 {
-    println!("either 3 or 9");
+    return "Y"
 }
 
-#[allow(unused_variables)]
-fn or_3_9_6(img: &IMAGE, iter:u32)
+
+fn or_3_9_6(img: &IMAGE, iter:u32) -> &str
 {
     let first_black = get_first_pixel(img, iter);
 
     if img.get_pixel(first_black.0 + 5, first_black.1) == &BLACK_PIXEL 
         && img.get_pixel(first_black.0 + 5, first_black.1 + 1) != &BLACK_PIXEL 
     {
-        println!("6 \t")
+        return "6"
     }
     else if img.get_pixel(first_black.0 + 5, first_black.1) == &BLACK_PIXEL 
             && img.get_pixel(first_black.0 + 5, first_black.1 + 1) == &BLACK_PIXEL 
     {
-        println!("9 \t")
+        return "9"
     }
     else
     {
-        println!("3 \t")
+        return "3"
     }
 }
 
 
-fn or_5_6(img: &IMAGE, iter:u32)
+fn or_5_6(img: &IMAGE, iter:u32) -> &str
 {
     let first_black = get_first_pixel(img, iter);
 
     if img.get_pixel(first_black.0, first_black.1 + 4) == &BLACK_PIXEL 
         && img.get_pixel(first_black.0, first_black.1 + 5) != &BLACK_PIXEL
     {
-        println!("5 \t")
+        return "5"
     }
     else
     {
-        println!("6 \t")
+        return "6"
     }
 }
 
 
-fn or_3_8_7(img: &IMAGE, iter:u32)
+fn or_3_8_7(img: &IMAGE, iter:u32) -> &str
 {
     let first_black = get_first_pixel(img, iter);
 
     if img.get_pixel(first_black.0 + 5, first_black.1 + 4) == &BLACK_PIXEL 
         && img.get_pixel(first_black.0 + 6, first_black.1 + 3) == &BLACK_PIXEL 
     {
-        println!("7 \t")
+        return "7"
     }
     else if img.get_pixel(first_black.0 + 1, first_black.1) == &BLACK_PIXEL 
     {
-        println!("3 \t")
+        return "3"
     }
     else
     {
-        println!("8 \t")
+        return "8"
     }
 }
 
 
-fn or_9_6(img: &IMAGE, iter:u32)
+fn or_9_6(img: &IMAGE, iter:u32) -> &str
 {
     let first_black = get_first_pixel(img, iter);
 
     if img.get_pixel(first_black.0 + 6, first_black.1 + 1) != &BLACK_PIXEL 
     {
-        println!("6 \t")
+        return "6"
     }
     else
     {
-        println!("9 \t")
+        return "9"
     }
+}
+
+
+
+#[cfg(test)]
+mod captcha
+{
+    use super::*;
+
+    fn _main(img: IMAGE) -> String
+    {
+        let nb_black = read_image(&img);
+        let mut result: String = "".to_owned();
+
+        for i in 0 .. 5
+        {
+            let nb : &str = get_nb(nb_black[i], &img, i as u32);
+            result.push_str(nb);
+        }
+
+        return result;
+    }
+
+    #[test]
+    fn _1_15524()
+    {
+        let img = open(r".\captchas\1-15524.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "15524");
+    }
+
+    #[test]
+    fn _2_31868()
+    {
+        let img = open(r".\captchas\2-31868.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "31868");
+    }
+
+
+    #[test]
+    fn _3_36152()
+    {
+        let img = open(r".\captchas\3-36152.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "36152");
+    }
+
+
+
+    #[test]
+    fn _4_46694()
+    {
+        let img = open(r".\captchas\4-46694.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "46694");
+    }
+
+
+    #[test]
+    fn _5_58865()
+    {
+        let img = open(r".\captchas\5-58865.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "58865");
+    }
+
+
+    #[test]
+    fn _6_77164()
+    {
+        let img = open(r".\captchas\6-77164.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "77164");
+    }
+
+    #[test]
+    fn _7_27361()
+    {
+        let img = open(r".\captchas\7-27361.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "27361");
+    }
+
+    #[test]
+    fn _8_63124()
+    {
+        let img = open(r".\captchas\8-63124.png").expect("can not find the image").into_rgb8();
+
+        let result = _main(img);
+
+        assert_eq!(result, "63124");
+    }
+
+    
 }
