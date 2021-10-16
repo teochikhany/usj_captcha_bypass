8cc6531d8ac3dca25837558ad0be0f73e7c0b8ed
Teo Chikhany <47039131+teochikhany@users.noreply.github.com>
2021-10-16 14:59:14 +0300

    remove unused dependencies and old code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 extern crate image;
-extern crate hex;
 
-use std::process::Command;
-use std::str;
-use wait_timeout::ChildExt;
-use std::time::Duration;
+
 use image::open;
 use std::fs::File;
 use std::io::prelude::*;
@@ -17,17 +13,6 @@ const BLACK_PIXEL: image::Rgb<u8> = image::Rgb([51u8, 51u8, 51u8]);
 
 
 fn main() {
-    
-    // println!("Hello, world!");
-
-    // start_capture();
-
-    // println!("finished waiting");
-
-    // let tshark_analyse = parse_pcap();
-
-    // extract_image(tshark_analyse);
-
 
     // Getting the image saved in the Clipboard
     let bitmap: Vec<u8> = get_clipboard(formats::Bitmap).expect("Not bitmap format");
@@ -49,51 +34,7 @@ fn main() {
     }
 }
 
-/// Start the capture with tshark using the following command :
-///     .\tshark.exe -i wifi -w AAA.pcap -f "tcp and host 193.227.187.169"
-#[allow(dead_code)]
-fn start_capture()
-{
-    let _tshark_capture = Command::new("D:\\Apps\\wireshark\\tshark.exe")
-                            .args(["-i", "wifi", "-w", "BAA.pcap", "-f", "tcp and host 193.227.187.169" ])
-                            .spawn()
-                            .expect("tshark command failed to start")
-                            .wait_timeout(Duration::from_secs(10));
-}
 
-
-/// Parse the pcap file with tshark using the following command
-/// D:\\Apps\\wireshark\\tshark.exe -r .\BAA.pcap -T json -Y 'http' -x > test.json
-#[allow(dead_code)]
-fn parse_pcap() -> std::process::Output
-{
-    return Command::new("D:\\Apps\\wireshark\\tshark.exe")
-                    .args(["-r", "BAA.pcap", "-T", "json", "-Y", "http", "-x"])
-                    .output()
-                    .expect("tshark command failed to start");
-}
-
-
-/// read the json file parsed from the pcap and 
-/// save the latest image with the dimentions of 120x30
-#[allow(dead_code)]
-fn extract_image(tshark_analyse: std::process::Output)
-{
-    let res = str::from_utf8(&tshark_analyse.stdout).expect("could not cast output as utf8");
-
-    let responce = json::parse(res).expect("could not parse the json file");
-
-    // println!("responce: {}", responce[1]["_source"]["layers"]["png_raw"][0]);
-
-    let image = &responce[1]["_source"]["layers"]["png_raw"][0].as_str().unwrap();
-
-    let buffer = hex::decode(image).expect("Decoding failed");
-
-    // println!("responce2 : {:?}", &buffer);
-
-    let mut file = File::create("captcha.png").expect("could do create file");
-    file.write_all(&buffer).expect("could not write to file");
-}
 
 
 /// Count how many black pixels vertically, from row 10 to 20 and adding 20 rows for each number
